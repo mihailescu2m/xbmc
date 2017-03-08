8db9e5a9097eb029eedbb8a6ad8f98c4ee0bc07d
Max Kellermann <max@duempel.org>
2017-03-08 13:26:21 +0100

    Wayland: replace Boost with C++11 standard library
    Signed-off-by: memeka <mihailescu2m@gmail.com>

--- xbmc/windowing/wayland/Keyboard.cpp
+++ xbmc/windowing/wayland/Keyboard.cpp
@@ -21,8 +21,6 @@
 #include <iostream>
 #include <stdexcept>
 
-#include <boost/bind.hpp>
-#include <boost/function.hpp>
 #include <boost/scope_exit.hpp>
 
 #include <wayland-client.h>
@@ -53,6 +51,12 @@ void DestroyXKBCommonContext(struct xkb_context *context,
 }
 }
 
+void
+xw::Keyboard::XkbContextDeleter::operator()(struct xkb_context *c)
+{
+  DestroyXKBCommonContext(c, m_xkbCommonLibrary);
+}
+
 xw::Keyboard::Keyboard(IDllWaylandClient &clientLibrary,
                        IDllXKBCommon &xkbCommonLibrary,
                        struct wl_keyboard *keyboard,
@@ -60,9 +64,7 @@ xw::Keyboard::Keyboard(IDllWaylandClient &clientLibrary,
   m_clientLibrary(clientLibrary),
   m_xkbCommonLibrary(xkbCommonLibrary),
   m_xkbCommonContext(CXKBKeymap::CreateXKBContext(m_xkbCommonLibrary),
-                     boost::bind(DestroyXKBCommonContext,
-                                 _1,
-                                 boost::ref(m_xkbCommonLibrary))),
+                     XkbContextDeleter(xkbCommonLibrary)),
   m_keyboard(keyboard),
   m_reciever(receiver)
 {
