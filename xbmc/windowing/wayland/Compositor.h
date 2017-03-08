916bdf4c43446b6110ea37a3f0c530132a66f548
Max Kellermann <max.kellermann@gmail.com>
2017-03-08 13:26:22 +0100

    windowing/wayland/Compositor: inline trivial methods
    Signed-off-by: memeka <mihailescu2m@gmail.com>

--- xbmc/windowing/wayland/Compositor.h
+++ xbmc/windowing/wayland/Compositor.h
@@ -19,9 +19,8 @@
  *  <http://www.gnu.org/licenses/>.
  *
  */
-struct wl_compositor;
-struct wl_surface;
-struct wl_region;
+
+#include <wayland-client.h>
 
 namespace xbmc
 {
@@ -31,23 +30,35 @@ class Compositor
 {
 public:
 
-  explicit Compositor(struct wl_compositor *compositor);
-  ~Compositor();
+  explicit Compositor(struct wl_compositor *compositor) :
+    m_compositor(compositor)
+  {
+  }
+
+  ~Compositor() {
+    wl_compositor_destroy(m_compositor);
+  }
 
   Compositor(const Compositor &) = delete;
   Compositor &operator=(const Compositor &) = delete;
 
-  struct wl_compositor * GetWlCompositor();
+  struct wl_compositor * GetWlCompositor() {
+    return m_compositor;
+  }
   
   /* Creates a "surface" on the compositor. This is not a renderable
    * surface immediately, a renderable "buffer" must be bound to it
    * (usually an EGL Window) */
-  struct wl_surface * CreateSurface() const;
+  struct wl_surface * CreateSurface() const {
+    return wl_compositor_create_surface(m_compositor);
+  }
   
   /* Creates a "region" on the compositor side. Server side regions
    * are manipulated on the client side and then can be used to
    * affect rendering and input on the server side */
-  struct wl_region * CreateRegion() const;
+  struct wl_region * CreateRegion() const {
+    return wl_compositor_create_region(m_compositor);
+  }
 
 private:
 
